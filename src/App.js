21d2e59fef129a17a8c5719dbb3d72b992db1624
Alena Buiko <abuiko.ma@gmail.com>
2021-11-16 21:57:36 -0500

    calculate number of words from textarea

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ function App() {
         setInputData(prevData => ({ ...prevData, text: value }))
     }
 
+    const calculateWords = text => {
+        const words = text.trim().split(" ");
+        return words.filter(word => word !== "")
+    }
+
     return (
         <div>
             <h1 className="title">How fast can you type?</h1>
             <textarea onChange={handleInput} value={inputData.text} />
             <h4>Time Remaining</h4>
-            <button className="startButton">Start</button>
+            <button className="startButton" onClick={() => calculateWords(inputData.text)}>Start</button>
             <h1 className="result">Result</h1>
         </div>
     )
