5d3d4452e853ebf8eac9ad3d135e1c6aaf49e7f3
Alena Buiko <abuiko.ma@gmail.com>
2021-11-16 22:57:19 -0500

    count / display word count when time is out

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ function App() {
     const [inputData, setInputData] = useState({ text: "" })
     const [timeRemaining, setTimeRemaining] = useState(5)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
 
     const handleInput = event => {
         const { value } = event.target
         setInputData(prevData => ({ ...prevData, text: value }))
     }
 
-    // const calculateWords = text => {
-    //     const words = text.trim().split(" ");
-    //     return words.filter(word => word !== "")
-    // }
+    const calculateWords = text => {
+        const words = text.trim().split(" ");
+        return words.filter(word => word !== "").length
+    }
 
     useEffect(() => {
         if (timeRemaining > 0 && isTimeRunning) {
             setTimeout(() => { setTimeRemaining(time => time - 1) }, 1000)
+        } else if (timeRemaining === 0) {
+            setIsTimeRunning(false)
+            setWordCount(calculateWords(inputData.text))
         }
     }, [timeRemaining, isTimeRunning])
     return (
@@ -26,7 +30,7 @@ function App() {
             <textarea onChange={handleInput} value={inputData.text} />
             <h4>Time Remaining: {timeRemaining}</h4>
             <button className="startButton" onClick={() => setIsTimeRunning(true)}>Start</button>
-            <h1 className="result">Result</h1>
+            <h1 className="result">Word Count: {wordCount}</h1>
         </div>
     )
 }
