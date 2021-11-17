baf4fb5b43387c450d9a67c29d65a0d09227ed36
Alena Buiko <abuiko.ma@gmail.com>
2021-11-16 22:39:58 -0500

    click btn to start timer

--- src/App.js
+++ src/App.js
@@ -3,28 +3,29 @@ import React, { useState, useEffect } from 'react'
 function App() {
     const [inputData, setInputData] = useState({ text: "" })
     const [timeRemaining, setTimeRemaining] = useState(5)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
 
     const handleInput = event => {
         const { value } = event.target
         setInputData(prevData => ({ ...prevData, text: value }))
     }
 
-    const calculateWords = text => {
-        const words = text.trim().split(" ");
-        return words.filter(word => word !== "")
-    }
+    // const calculateWords = text => {
+    //     const words = text.trim().split(" ");
+    //     return words.filter(word => word !== "")
+    // }
 
     useEffect(() => {
-        if (timeRemaining > 0) {
+        if (timeRemaining > 0 && isTimeRunning) {
             setTimeout(() => { setTimeRemaining(time => time - 1) }, 1000)
         }
-    })
+    }, [timeRemaining, isTimeRunning])
     return (
         <div>
             <h1 className="title">How fast can you type?</h1>
             <textarea onChange={handleInput} value={inputData.text} />
             <h4>Time Remaining: {timeRemaining}</h4>
-            <button className="startButton" onClick={() => calculateWords(inputData.text)}>Start</button>
+            <button className="startButton" onClick={() => setIsTimeRunning(true)}>Start</button>
             <h1 className="result">Result</h1>
         </div>
     )
