8cafb4ee8195e715498aa219a873b33132c3aae5
Alena Buiko <abuiko.ma@gmail.com>
2021-11-16 23:28:40 -0500

    refresh textarea / timer when click start btn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
 function App() {
-    const [inputData, setInputData] = useState({ text: "" })
-    const [timeRemaining, setTimeRemaining] = useState(5)
+    const STARTING_TIME = 10
+
+    const [inputData, setInputData] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
     const [wordCount, setWordCount] = useState(0)
 
     const handleInput = event => {
         const { value } = event.target
-        setInputData(prevData => ({ ...prevData, text: value }))
+        setInputData(value)
     }
 
     const calculateWords = text => {
@@ -16,20 +18,30 @@ function App() {
         return words.filter(word => word !== "").length
     }
 
+    const startGame = () => {
+        setIsTimeRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setInputData("")
+    }
+
+    const endGame = () => {
+        setIsTimeRunning(false)
+        setWordCount(calculateWords(inputData))
+    }
+
     useEffect(() => {
         if (timeRemaining > 0 && isTimeRunning) {
             setTimeout(() => { setTimeRemaining(time => time - 1) }, 1000)
         } else if (timeRemaining === 0) {
-            setIsTimeRunning(false)
-            setWordCount(calculateWords(inputData.text))
+            endGame()
         }
     }, [timeRemaining, isTimeRunning])
     return (
         <div>
             <h1 className="title">How fast can you type?</h1>
-            <textarea onChange={handleInput} value={inputData.text} />
+            <textarea onChange={handleInput} value={inputData} />
             <h4>Time Remaining: {timeRemaining}</h4>
-            <button className="startButton" onClick={() => setIsTimeRunning(true)}>Start</button>
+            <button className="startButton" onClick={startGame}>Start</button>
             <h1 className="result">Word Count: {wordCount}</h1>
         </div>
     )
