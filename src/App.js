29164538dbb1bcb3ce6966c8cf7471995f46c463
Alena Buiko <abuiko.ma@gmail.com>
2021-11-16 21:09:08 -0500

    import css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react'
 function App() {
     return (
         <div>
-            <h1>Title</h1>
+            <h1 className="title">Title</h1>
             <textarea />
             <h4>Time Remaining</h4>
-            <button>Start</button>
-            <h1>Result</h1>
+            <button className="startButton">Start</button>
+            <h1 className="result">Result</h1>
         </div>
     )
 }
