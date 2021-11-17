49b828abe1bb0478e81de5db24edee1f84140a4a
Alena Buiko <abuiko.ma@gmail.com>
2021-11-16 21:32:21 -0500

    track state of input in textarea

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+    const [inputData, setInputData] = useState({ text: "" })
+
+    const handleInput = event => {
+        const { value } = event.target
+        setInputData(prevData => ({ ...prevData, text: value }))
+    }
+
     return (
         <div>
-            <h1 className="title">Title</h1>
-            <textarea />
+            <h1 className="title">How fast can you type?</h1>
+            <textarea onChange={handleInput} value={inputData.text} />
             <h4>Time Remaining</h4>
             <button className="startButton">Start</button>
             <h1 className="result">Result</h1>
